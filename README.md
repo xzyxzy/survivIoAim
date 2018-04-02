c901f0480b982f5fee219a24774637c7881c92b4
w3x731 <mredwardprise@gmail.com>
2018-04-02 10:11:44 +0300

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# survivIoAim
+## How to use
+1. Open surviv.io game tab in chrome or chromium
+2. Press F12, you would see dev panel. Switch to "sources" tab on top.
+3. On the left side column switch to the network tab and open folder "top/surviv.io/js/app.x.js" where x is any combination of symbols. Because code is heavy there may exists some lags.
+4. Look at the down side and find "{}" symbol. Press it. Over time you must see new opened tab with a pretty viewed code.
+5. Ok, select all the code (ctrl + a) and copy it to the sublime text editor (this editor shows the lines number).
+6. Find the `case p.Msg.Update:` line. Look at the number of this line and remember.
+7. Come back to the pretty view code (chrome). Scroll to the remembered line number. And click left side number of this line. Number of the line must switch to blue color.
+8. Ok, start new solo game. Come back to the code, on the right side you must see "Paused on breakpoint". Below in the scope tab find variable "this". Click it right mouse key, and choose "Store as global variable" in context menu.
+9. In the console of the the bottom you must see `temp1`.
+10. Deactivate breakpoints and resume script execution (press ctrl + f8 and after press f8).
+11. Write in the console `var game = temp1`. Then paste [this](http://github.com) code.
+12. In the console you may type `reload()` for launch aim and `stop()` for stops the aim.
+
+Its must be worked. How to check script? Your person must automatically rotate to the closest enemy.
+
+If page has been updated, is necessary repeat this steps again.
+
+All actions you take at your own risk. The author is not responsible for the consequences of your actions.
---
+++ survivIoAim.js
@@ -0,0 +1,104 @@
+
+var detectEnemies = function() {
+	var result = [];
+	if(!game.playerBarn.playerInfo[game.activeId]) return result;
+
+	var selfTeamId = game.playerBarn.playerInfo[game.activeId].teamId;
+	var selfId = game.activeId;
+	var objectIds = Object.keys(game.objectCreator.idToObj);
+	var playerIds = Object.keys(game.playerBarn.playerInfo);
+	for(var i = 0; i < playerIds.length; i++) {
+		if( game.objectCreator.idToObj[playerIds[i]] && 
+			(!game.objectCreator.idToObj[playerIds[i]].netData.dead) && 
+			game.playerBarn.playerInfo[playerIds[i]].teamId != selfTeamId) {
+			if(playerIds[i] != selfId) {
+				result.push(game.objectCreator.idToObj[playerIds[i]]);	
+			}
+		}
+	}
+
+	return result;
+}
+
+var getSelfPos = function() {
+	if(game.activePlayer) {
+		return game.activePlayer.pos
+	} else {
+		return false;
+	}
+}
+
+var calculateRadianAngle = function(cx, cy, ex, ey) {
+  var dy = ey - cy;
+  var dx = ex - cx;
+  var theta = Math.atan2(dy, dx); // range (-PI, PI]
+  // theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
+  // if (theta < 0) theta = 360 + theta; // range [0, 360)
+
+  return theta;
+}
+
+var getMinimalDistanceIndex = function(enemyDistances) {
+	return enemyDistances.indexOf(Math.min.apply(null, enemyDistances));
+}
+
+var iterate = function() {
+	var enemyDistances = [];
+	var enemyRadianAngles = [];
+	var enemies = detectEnemies();
+	var selfPos = getSelfPos();
+
+	if(!selfPos) return;
+
+	for(var i = 0; i < enemies.length; i++) {
+		var enemyPos = enemies[i].pos;
+		var distance = Math.sqrt(Math.pow(Math.abs(selfPos.x - enemyPos.x), 2) + Math.pow(Math.abs(selfPos.y - enemyPos.y), 2));
+		var radianAngle = calculateRadianAngle(selfPos.x, selfPos.y, enemyPos.x, enemyPos.y);
+
+		enemyDistances.push(distance);
+		enemyRadianAngles.push(radianAngle);
+	}
+
+	var minimalDistanceIndex = getMinimalDistanceIndex(enemyDistances);
+
+	// Delete self distance index
+	// enemyDistances.splice(minimalDistanceIndex, 1);
+	// enemyRadianAngles.splice(minimalDistanceIndex, 1);
+	
+	var minimalDistanceIndex = getMinimalDistanceIndex(enemyDistances);	
+	var minimalDistance = enemyDistances[minimalDistanceIndex];
+	var minimalDistanceRadianAngle = enemyRadianAngles[minimalDistanceIndex];
+
+	var halfScreenWidth = game.camera.screenWidth/2;
+	var halfScreenHeight = game.camera.screenHeight/2;
+
+	var minScreenCircleRadius = halfScreenHeight > halfScreenWidth ? halfScreenWidth : halfScreenHeight;
+	minScreenCircleRadius = Math.floor(minScreenCircleRadius - 1);
+
+	var targetMousePosition = {};
+	targetMousePosition.x = halfScreenWidth + minScreenCircleRadius * Math.cos(minimalDistanceRadianAngle);
+	targetMousePosition.y = halfScreenHeight - minScreenCircleRadius * Math.sin(minimalDistanceRadianAngle);
+
+	if(targetMousePosition.x && targetMousePosition.y) {
+		game.input.mousePos = {
+			x: targetMousePosition.x,
+			y: targetMousePosition.y,
+		}
+	}
+}
+
+var timer = {};
+function ticker() {
+	iterate();
+	console.log("Iterate");
+	timer = setTimeout(ticker, 30);
+}
+
+function reload() {
+	if(timer) clearTimeout(timer);
+	ticker();
+}
+
+function stop() {
+	if(timer) clearTimeout(timer);
+}
